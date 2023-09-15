0eaa83da060f527a20f09223f3a5602e31a09996
ryudg <ryudg95@gmail.com>
2023-09-16 04:45:41 +0900

    LearningTS 2장 Type System

--- 2.TypeSystem/index.js
+++ 2.TypeSystem/index.js
@@ -1,80 +1,35 @@
 /* ----------------------------------------------------------------------- */
-
-let singer = "10cm";
+var singer = "10cm";
 // singer는 string 타입이라고 추론된다.
 // 하지만 singer를 let이 아닌 const로 선언하면, singer는 "Kim"이라는 값을 가지는 변수가 된다.
-
 /* ----------------------------------------------------------------------- */
-
-let bestSong = Math.random() > 0.5 ? "폰서트" : "부동의 첫사랑";
-
+var bestSong = Math.random() > 0.5 ? "폰서트" : "부동의 첫사랑";
 /* ----------------------------------------------------------------------- */
-
-let firstName1 = 'Kim'
+var firstName1 = 'Kim';
 firstName1.length();
 //        ^^^^^^ This expression is not callable.
 //               Type 'Number' has no call signatures.
-
 /* ----------------------------------------------------------------------- */
-
-let let wat; 
+var let, wat;
 //      ^^^ ',' expected.
-
 /* ----------------------------------------------------------------------- */
-
 console.blub();
 //      ^^^^ Property 'blub' does not exist on type 'Console'.
-
 /* ----------------------------------------------------------------------- */
-
-let firstName2 = 'Kim'
-firstName2 = "Lee";
-
+var firstName = 'Kim';
+firstName = "Lee";
 /* ----------------------------------------------------------------------- */
-
-let lastName = "Cheolsoo";
-   lastName = true;
+var lastName = "Cheolsoo";
+lastName = true;
 // ^^^^^^^^ Type 'boolean' is not assignable to type 'string'.
-
 /* ----------------------------------------------------------------------- */
-
-let user1; // type: any
+var user1; // type: any
 user1 = "Lee"; // type: string
 user1.toUpperCase();
-
 user1 = 123.456; // type: number
 user1.toPrecision(1); // toPrecision()은 number 타입의 메소드이며, 소수점 이하의 자릿수를 지정한 만큼 표시한다.
-
 user1.toUpperCase();
 //   ^^^^^^^^^^^ Property 'toUpperCase' does not exist on type 'number'.
-
 /* ----------------------------------------------------------------------- */
-
-let user2: string;
-user2 = "Lee";
-
-/* ----------------------------------------------------------------------- */
-
-
-let user: string;
-   user = 123;
-// ^^^^ Type 'number' is not assignable to type 'string'.
-
-/* ----------------------------------------------------------------------- */
-
-let firstName3: string = "Kim";
-
-/* ----------------------------------------------------------------------- */
-
-let firstName: string = 123;
-//  ^^^^^^^^^^ Type 'number' is not assignable to type 'string'.
-
-/* ----------------------------------------------------------------------- */
-
-let rapper = "Eminem";
-rapper.length;
-
-rapper.push('!');
-//     ^^^^ Property 'push' does not exist on type 'string'.
-
-/* ----------------------------------------------------------------------- */
+var user;
+user = "Lee";
--- README.md
+++ README.md
@@ -6,6 +6,6 @@ TypeScript 공식문서와 러닝 타입스크립트를 통해 타입스크립�
 
 ## 목차
 
-1. 📝 TypeScript란? [Code](https://github.com/ryudg/TypeScriptStudy/tree/LearningTS/1.HelloTS) [Notion](https://likeliontypescript.notion.site/0-TypeScript-a1fd13a8db224ff0b62f121973626601#bfa7a1727d564f398817a31827956bd9)
-2. 📝 Type System [Code]() [Notion](https://www.notion.so/likeliontypescript/2-Type-System-625d500d19dd4618a24fc35e42675569)
-3.
+1. 📝 TypeScript란? [Code](https://github.com/ryudg/TypeScriptStudy/tree/LearningTS/1.HelloTS) [Notion]()
+2. 📝 Type System [Code](https://github.com/ryudg/TypeScriptStudy/tree/LearningTS/2.TypeSystem) [Notion]()
+3. 📝 Union & Literal [Code]() [Notion]()
